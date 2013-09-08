1068bb6be275999e75bc3101beb3bea4e5ac229c
Laurens Ramandt <laurens.ramandt@live.be>
2013-09-08 22:33:21 +0200

    New websites are now immediately active without delay.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var ejs = require('ejs');
 var cody = require('./cody');
 
 cody.server = express();
+cody.instance = null;
 
 // cookies are encrypted, so we need a pass phrase
 cody.server.use(express.bodyParser());
@@ -32,47 +33,51 @@ cody.server.all("/cody/*", function (req, res) {
 });
 
 
-// startup all the web applications
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "cody", password: "ydoc",
-  database: "cody"
-})
-
-connection.connect();
-
-connection.query("SELECT name, version, dbuser, dbpassword, dbhost, datapath, db, hostname FROM websites WHERE active='Y' AND ownerconfirmed='Y' ORDER BY id", function(err, rows, fields) {
-    if(err) throw err;
-    cody.Application.each(rows, function(next){
-      var row = this;
-      console.log(row);
-
-      cody.startWebApp(cody.server, {
-            "name": row.name,
-            "version": row.version,
-            "hostnames" : row.hostname,
-            "dbuser": row.dbuser,
-            "dbpassword": row.dbpassword,
-            "dbhost": row.dbhost,
-            "datapath": row.datapath,
-            "db": row.db,
-            "controllers": require("./" + row.name + "/controllers/")
-        }, next);
-
-    }, function() {
-      console.log("Loaded all apps....");
-      connection.end();
-
-      cody.server.listen(3000);
-      console.log('Listening on port 3000');
+cody.bootstrap = function () {
+    // startup all the web applications
+    var connection = mysql.createConnection({
+        host: "localhost",
+        user: "cody", password: "ydoc",
+        database: "cody"
     });
-});
 
+    connection.connect();
 
+    connection.query("SELECT name, version, dbuser, dbpassword, dbhost, datapath, db, hostname FROM websites WHERE active='Y' AND ownerconfirmed='Y' ORDER BY id", function (err, rows, fields) {
+        if (err) throw err;
+        cody.Application.each(rows, function (next) {
+            var row = this;
+            console.log(row);
 
-if (!process.stderr.isTTY) {
-    process.on('uncaughtException', function (err) {
-        console.error('Uncaught exception : ' + err.stack);
+            cody.startWebApp(cody.server, {
+                "name": row.name,
+                "version": row.version,
+                "hostnames": row.hostname,
+                "dbuser": row.dbuser,
+                "dbpassword": row.dbpassword,
+                "dbhost": row.dbhost,
+                "datapath": row.datapath,
+                "db": row.db,
+                "controllers": require("./" + row.name + "/controllers/")
+            }, next);
+
+        }, function () {
+            console.log("Loaded all apps....");
+            connection.end();
+
+        });
+
+        if(cody.instance !== null){
+            cody.instance.close();
+        }
+        cody.instance = cody.server.listen(3000); //cody.server is an instance of express()
+        console.log('Listening on port 3000');
     });
-}
+
+    if (!process.stderr.isTTY) {
+        process.on('uncaughtException', function (err) {
+            console.error('Uncaught exception : ' + err.stack);
+        });
+    }
+};
+cody.bootstrap();
