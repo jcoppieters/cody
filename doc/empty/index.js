60560e578b373f6b43d1be6913f940feb6f313db
johan <johan.coppieters@howest.be>
2015-04-10 17:44:40 +0200

    better install, startup script outside of app dir, ...

--- doc/empty/index.js
+++ doc/empty/index.js
@@ -1,25 +1,25 @@
+/**
+ * Created by johan on 25/03/15.
+ */
 //
 // Johan Coppieters - jan 2013 - Cody CMS
 //
 // empty website for Cody CMS
 //
-// v1.0 -- was running from within the web dir
-// v1.1 -- now running from the dir above... using the config from within...
 //
 
-var cody = require('cody');
+var cody = require("cody");
 var express = cody.express;
 var fs = cody.fs;
-var mysql = cody.mysql;
 var path = require("path");
 
-var ejs = cody.ejs;
-var sitename = __filename.split("/").pop().split(".")[0];
 
 cody.server = express();
 var bodyParser = cody.bodyParser;
 var expressSession = cody.expressSession;
 var multer = cody.multer;
+
+var sitename = __filename.split("/").pop().split(".")[0];
 
 
 // use the new 4.x middleware
@@ -28,13 +28,19 @@ cody.server.use(expressSession({secret: 'a secret', cookie: { maxAge: 60*60*1000
 cody.server.use(bodyParser.urlencoded({ extended: true }));
 cody.server.use(multer());
 
+
 // startup a routing for all static content of cody (images, javascript, css)
 cody.server.get("/cody/static/*", function (req, res) {
     var fileserver = new cody.Static(req, res, "");
     fileserver.serve();
 });
 
-// setup the config. Order of importance: config.json < -c command line config < environment values
+
+// setup the config.
+//  Order of importance:
+//  1. config.json >> 2. -c command line config >> 3. environment values
+
+
 // 1. load default config
 cody.config = require(path.join(__dirname, sitename, "config.json"));
 cody.config.controllers = require(path.join(__dirname, sitename, "controllers"));
@@ -53,15 +59,16 @@ Object.keys(cody.config).forEach(function (name) {
   cody.config[name] = process.env[name] || cody.config[name];
 });
 
-// startup the web server
+
+// start the web app with all collected params
 cody.startWebApp(cody.server, cody.config, function() {
-    console.log("Loaded web app....");
-    console.log("with config (adjust in config.json or by setting as environment variables):")
-    console.log(cody.config);
-    var portNr = cody.config.port || 3001;
-    cody.server.listen(portNr);
-    console.log('Listening on port ' + portNr);
+  console.log("Loaded " + sitename + " web app with config:");
+  console.log(cody.config);
+  var portNr = cody.config.port || 3001;
+  cody.server.listen(portNr);
+  console.log("Listening on port " + portNr);
 });
+
 
 if (!process.stderr.isTTY) {
     process.on('uncaughtException', function (err) {
